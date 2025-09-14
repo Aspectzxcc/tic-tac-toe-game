cfc115bc675dd91d93572b116a6668e43bf54d10
aspectzxcc <josh10nathan@gmail.com>
2025-09-15 05:52:41 +0800

    added event listeners for room and game  gateway service

--- gateway/src/socket.ts
+++ gateway/src/socket.ts
@@ -4,6 +4,31 @@ export default function registerSocketHandlers(io: Server) {
   io.on('connection', (socket: Socket) => {
     console.log(`${socket.id} connected`);
 
+    socket.on('room:create', (callback) => {
+      const roomId = Math.random().toString(36).substring(2, 8);
+      socket.join(roomId);
+      callback(roomId);
+      console.log(`${socket.id} created room ${roomId}`);
+    });
+
+    socket.on('room:join', (roomId: string, callback) => {
+      const room = io.sockets.adapter.rooms.get(roomId);
+      if (room && room.size < 2) {
+        socket.join(roomId);
+        callback({ success: true });
+        console.log(`${socket.id} joined room ${roomId}`);
+        io.to(roomId).emit('room:ready');
+      } else {
+        callback({ success: false, message: 'Room is full or does not exist' });
+      }
+    });
+
+    socket.on('game:move', (data) => {
+      const { roomId, move } = data as { roomId: string; move: any };
+      socket.to(roomId).emit('game:move', move);
+      console.log(`${socket.id} made move in room ${roomId}`);
+    });
+
     socket.on('disconnect', () => {
       console.log(`${socket.id} disconnected`);
     });
