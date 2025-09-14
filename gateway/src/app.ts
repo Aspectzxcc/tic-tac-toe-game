63f608b9e6a5b4cae753d51a8f8a82405c16e77e
aspectzxcc <josh10nathan@gmail.com>
2025-09-15 04:33:05 +0800

    added socket io deps to gateway service

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -5,7 +5,8 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(200).json({ message: 'Welcome to the Quantum Tic-Tac-Toe Gateway!' });
 });
+
 
 export default app;
