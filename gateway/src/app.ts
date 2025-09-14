4352b102d90e283178d5f2285543dd71436503b1
aspectzxcc <josh10nathan@gmail.com>
2025-09-15 05:32:14 +0800

    added test frontend for gateway connections

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -1,17 +1,22 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import { join } from 'node:path';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(process.cwd(), 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log(socket.id, 'connected');
+});
+
+io.on('disconnect', (socket) => {
+  console.log(socket.id, 'disconnected');
 });
 
 export default server;
