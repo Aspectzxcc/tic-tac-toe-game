e24de7d32238eabe0c2b3f44129fcb52d1c7ed10
aspectzxcc <josh10nathan@gmail.com>
2025-09-15 04:20:30 +0800

    gateway server init

---
+++ gateway/server.ts
@@ -0,0 +1,7 @@
+import app from './src/app.ts';
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
