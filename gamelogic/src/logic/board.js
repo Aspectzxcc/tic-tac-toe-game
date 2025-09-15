283793d35851fbdb0544b3d89110ff2d67362a6f
Roku-22 <lukedainiel46@gmail.com>
2025-09-15 20:20:08 +0800

    created initial state for board (not final)

---
+++ gamelogic/src/logic/board.js
@@ -0,0 +1,13 @@
+let properties = {isEmpty: true, string:"", value: null};
+
+const initialState ={
+   board:[
+  [properties, properties, properties],
+  [properties, properties, properties],
+  [properties, properties, properties],
+  ],
+  playerTurn: null,
+  winner: null, 
+  isDraw: false
+};
+
