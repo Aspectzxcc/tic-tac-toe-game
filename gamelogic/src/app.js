24f85e04e8a52e89364810ede11bbbb13dd7bc72
Roku-22 <lukedainiel46@gmail.com>
2025-09-15 21:24:21 +0800

    gameRoutes test router done

--- gamelogic/src/app.js
+++ gamelogic/src/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
-
+const routes = require('./routes/index')
 
 
 const app = express();
 
+app.use('/api', routes);
+
+
 app.use('/',(req, res)=>{
   res.send("hello world")
 })
-
 
 
 module.exports = app;
